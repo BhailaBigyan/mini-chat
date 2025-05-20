3754fa9b50a5adacfd139ce6ca27161fbf319fb4
Bigyan Bhaila <bigyanbhaila98@gmail.com>
2025-05-21 05:00:07 +0545

    Initial chat app

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from "public" folder
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (data) => {
+        io.emit('chat message', {
+            name: data.name,
+            message: data.message
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
